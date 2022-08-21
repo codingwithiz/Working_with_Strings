515a4c09b9823e6d49ebc3f4a3830f7688fff883
codingwithiz <111691855+codingwithiz@users.noreply.github.com>
2022-08-21 22:26:22 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+
+phrase = "ing zhen is handsome"
+
+print(phrase.upper())
+print(phrase.lower())
+print(phrase.title())
+print(phrase.isupper())
+print(phrase.islower())
+print(len(phrase))
+print(phrase[5])
+print(phrase.index("handsome"))
+print(phrase.replace("handsome" , "lengzai"))
+
